970ee9cb7f5326f54fe0cf5e2cbc5d18d3c25ccf
Karin Rikkinen <karri1@users.noreply.github.com>
2016-10-12 21:06:30 +0300

    registeerinud kasutaja kantakse andmebaasi

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+require("../../config.php");
+
 function cleanInput($input){
 		
 		$input = trim($input);
@@ -9,10 +11,26 @@ function cleanInput($input){
 		
 	}
 	
-function signUp($firstname, $lastname, $email, $password, $gender, $newsletter){
-	
-//ÜHENDUS
+function signUp($firstname, $lastname,  $gender, $address, $city, $zip, $email, $password){
+	$database = "if16_karin";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $database);
+		
+	$stmt = $mysqli->prepare("INSERT INTO users_katse (Eesnimi, Perekonnanimi, Sugu, Aadress, Asula, Sihtnumber, Email, Password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+	echo $mysqli -> error;   
+		
+	$stmt -> bind_param("ssssssss", $firstname, $lastname, $gender, $address, $city, $zip, $email, $password ); 
 
+	if ($stmt->execute()) {
+		echo "<br> Kasutaja loodud<br> <br>";
+		echo "Nimi: ". $firstname . " " . $lastname . "<br>";	
+		echo "E-post: ". $email . "<br>";
+		echo "Aadress: ". $address . " " . $city . ", " . $zip . "<br>";
+	} else {
+		echo "ERROR ".$stmt->error;
+	}
+			
+	$stmt->close();
+	$mysqli->close();
 }
 
 function login($loginEmail, $loginPassword){
